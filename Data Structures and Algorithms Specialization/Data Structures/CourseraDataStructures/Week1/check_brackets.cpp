ed5147f5c5dc5e1c410ecd1afeb4788bea13ecb0
Katy <katybucsa@github.com>
2021-01-09 23:13:01 +0200

    add Data Structures course work for week 1 of study

---
+++ Data Structures and Algorithms Specialization/Data Structures/CourseraDataStructures/Week1/check_brackets.cpp
@@ -0,0 +1,56 @@
+//#include <iostream>
+//#include <stack>
+//#include <string>
+//
+//struct Bracket {
+//	Bracket(char type, int position) :
+//		type(type),
+//		position(position)
+//	{}
+//
+//	bool Matchc(char c) {
+//		if (type == '[' && c == ']')
+//			return true;
+//		if (type == '{' && c == '}')
+//			return true;
+//		if (type == '(' && c == ')')
+//			return true;
+//		return false;
+//	}
+//
+//	char type;
+//	int position;
+//};
+//
+//int main() {
+//
+//	std::string text;
+//	getline(std::cin, text);
+//
+//	std::stack <Bracket> opening_brackets_stack;
+//	for (int position = 0; position < text.length(); ++position) {
+//		char next = text[position];
+//
+//		if (next == '(' || next == '[' || next == '{') {
+//			opening_brackets_stack.push(Bracket(next, position));
+//		}
+//
+//		if (next == ')' || next == ']' || next == '}') {
+//			if (opening_brackets_stack.empty()) {
+//				std::cout << position + 1;
+//				return 0;
+//			}
+//			char top = opening_brackets_stack.top().type;
+//			if ((next == ')' && top != '(') || (next == ']' && top != '[') || (next == '}' && top != '{')) {
+//				std::cout << position + 1;
+//				return 0;
+//			}
+//			opening_brackets_stack.pop();
+//		}
+//	}
+//	if (!opening_brackets_stack.empty())
+//		std::cout << opening_brackets_stack.top().position + 1;
+//	else
+//		std::cout << "Success";
+//	return 0;
+//}
---
+++ Data Structures and Algorithms Specialization/Data Structures/CourseraDataStructures/Week1/max_sliding_window.cpp
@@ -0,0 +1,45 @@
+//#include <iostream>
+//#include <vector>
+//#include <deque>
+//
+//using std::cin;
+//using std::cout;
+//using std::vector;
+//
+//void max_sliding_window_naive(vector<int> const& A, int w) {
+//
+//	std::deque<int> window, maxx;
+//	for (size_t i = 0; i < A.size(); ++i) {
+//		window.push_back(A[i]);
+//		if (maxx.empty())
+//			maxx.push_back(A[i]);
+//		else {
+//			while (!maxx.empty() && maxx.back() < A[i])
+//				maxx.pop_back();
+//			maxx.push_back(A[i]);
+//		}
+//		if (i + 1 >= w) {
+//			cout << maxx.front() << " ";
+//			if (maxx.front() == window.front())
+//				maxx.pop_front();
+//			window.pop_front();
+//		}
+//	}
+//}
+//
+//
+//int main() {
+//	int n = 0;
+//	cin >> n;
+//
+//	vector<int> A(n);
+//	for (size_t i = 0; i < n; ++i)
+//		cin >> A.at(i);
+//
+//	int w = 0;
+//	cin >> w;
+//
+//	max_sliding_window_naive(A, w);
+//
+//	return 0;
+//}
